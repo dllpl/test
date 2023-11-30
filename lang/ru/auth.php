733069db0371f1b032fd641d57659727943a6682
root <root@host1863114-1.hostland.pro>
2023-11-30 17:47:47 +0300

    add from server

--- lang/ru/auth.php
+++ lang/ru/auth.php
@@ -1,41 +1,27 @@
-<?php
+<?php 
 
 return [
-    
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-    
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-    
-    'login' => 'Login',
-    'register' => 'Register',
-    'account_info' => 'Account info',
-    'change_info' => 'Change info',
-    'change_password' => 'Change password',
-    'logout' => 'Logout',
-    'remember_me' => 'Remember me',
-    'information_updated' => 'Your information has been updated.',
-    'name' => 'Name',
+    'failed' => 'Неверно введены логин/пароль',
+    'throttle' => 'Слишком много попыток. Попробуйте через :seconds секунд',
+    'login' => 'Войти',
+    'register' => 'Регистрация',
+    'account_info' => 'Аккаунт',
+    'change_info' => 'Изменить информацию',
+    'change_password' => 'Сменить пароль',
+    'logout' => 'Выйти',
+    'remember_me' => 'Запомнить меня',
+    'information_updated' => 'Ваша информация обновлена',
+    'name' => 'Имя',
     'email' => 'Email',
-    'password' => 'Password',
-    'confirm_password' => 'Confirm Password',
-    'old_password' => 'Old Password',
-    'new_password' => 'New Password',
-    'cancel' => 'Cancel',
+    'password' => 'Пароль',
+    'confirm_password' => 'Пароль подтверждение',
+    'old_password' => 'Старый пароль',
+    'new_password' => 'Новый пароль',
+    'cancel' => 'Отмена',
     'reset_password' => 'Reset Password',
     'send_password_reset_link' => 'Send Password Reset Link',
-    'forgot_password_question' => 'Forgot your password?',
-    'wrong_password' => 'The old password your entered is wrong.',
-    'password_updated' => 'Your password has been changed.',
-    'registration_closed' => 'Registration is closed.',
-
+    'forgot_password_question' => 'Забыли пароль?',
+    'wrong_password' => 'Старый пароль неверный.',
+    'password_updated' => 'Ваш пароль изменен!',
+    'registration_closed' => 'Регистрация закрыта.',
 ];
