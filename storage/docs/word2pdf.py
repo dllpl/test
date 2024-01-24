61ce120c2b7255fe7631c2d8d84681cfcc110293
Nikita Ivanov <nikita03.1998@mail.ru>
2024-01-24 09:43:47 +0300

    Правка

--- storage/docs/word2pdf.py
+++ storage/docs/word2pdf.py
@@ -3,7 +3,9 @@ import subprocess
 
 def docx_to_pdf(input_file, output_file):
     try:
-        subprocess.run(["unoconv", "-f", "pdf", "-o", output_file, input_file])
+        env = os.environ.copy()
+        env['PATH'] = '/bin:/usr/bin:/usr/local/bin'  # Add necessary paths to your PATH variable
+        subprocess.run(["unoconv", "-f", "pdf", "-o", output_file, input_file], env=env)
         print(f"File {input_file} converted to {output_file} successfully!")
     except Exception as e:
         print(f"Error converting {input_file} to PDF: {e}")
