59bdb03a43357eaad2d76eb35a7fcf9d805aa0c7
Nikita Ivanov <nikita03.1998@mail.ru>
2024-01-24 09:35:16 +0300

    Правка

--- storage/docs/word2pdf.py
+++ storage/docs/word2pdf.py
@@ -1,27 +1,13 @@
-import os
-import pdfkit
-from docx import Document
 import sys
+import subprocess
 
 def docx_to_pdf(input_file, output_file):
-    # Read the content of the docx file
-    doc = Document(input_file)
-    html_content = ""
-    for paragraph in doc.paragraphs:
-        html_content += f"<p>{paragraph.text}</p>"
+    try:
+        subprocess.run(["unoconv", "-f", "pdf", "-o", output_file, input_file])
+        print(f"File {input_file} converted to {output_file} successfully!")
+    except Exception as e:
+        print(f"Error converting {input_file} to PDF: {e}")
 
-    # Create a temporary HTML file
-    tmp_html_file = "/tmp/temp.html"
-    with open(tmp_html_file, "w", encoding="utf-8") as file:
-        file.write(html_content)
-
-    # Convert the HTML to PDF
-    pdfkit.from_file(tmp_html_file, output_file, options={'enable-local-file-access': None})
-
-    # Clean up the temporary HTML file
-    os.remove(tmp_html_file)
-
-# Example usage
 input_docx_file = sys.argv[1]
 output_pdf_file = sys.argv[2]
 docx_to_pdf(input_docx_file, output_pdf_file)
