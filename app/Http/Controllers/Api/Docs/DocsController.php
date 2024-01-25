4f0e0b36cb8a91929a530c32db175b1ce2539f08
dllpl <nick.iv.dev@gmail.com>
2024-01-25 22:39:58 +0300

    Правка

--- app/Http/Controllers/Api/Docs/DocsController.php
+++ app/Http/Controllers/Api/Docs/DocsController.php
@@ -48,7 +48,7 @@ class DocsController extends Controller
                         }
                     } else {
                         if ($key_2 === 'price') {
-                            $doc->setValue("$key_1.$key_2", number_format($value_2, 2, ',', ' ') . ' руб.');
+                            $doc->setValue("$key_1.$key_2",  !empty($value_2) ? number_format($value_2, 2, ',', ' ') . ' руб.' : '');
                             $doc->setValue("$key_1.price.bukov", '(' . $this->num2str($value_2) . ')');
                         } else {
                             $doc->setValue("$key_1.$key_2", $value_2);
