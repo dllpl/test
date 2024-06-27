ffd1fbf4cd0c38531cbd3595e6dfcca2f819ce80
dllpl <nick.iv.dev@gmail.com>
2024-06-27 22:24:48 +0300

    google api fix

--- app/Http/Controllers/Api/Auth/SocialController.php
+++ app/Http/Controllers/Api/Auth/SocialController.php
@@ -37,20 +37,20 @@ use Laravel\Socialite\Facades\Socialite;
 class SocialController extends BaseController
 {
 	use AuthenticatesUsers;
-	
+
 	// Supported Providers
 	// Stateless authentication is not available for the Twitter driver, which uses OAuth 1.0 for authentication.
 	private array $network = ['facebook', 'linkedin', 'google'];
 	private array $networkChecker;
 	private string $serviceError = "Unknown error. The service does not work.";
-	
+
 	/**
 	 * SocialController constructor.
 	 */
 	public function __construct()
 	{
 		parent::__construct();
-		
+
 		// Providers Checker
 		$this->networkChecker = [
 			'facebook' => (config('settings.social_auth.facebook_client_id') && config('settings.social_auth.facebook_client_secret')),
@@ -59,7 +59,7 @@ class SocialController extends BaseController
 			'google'   => (config('settings.social_auth.google_client_id') && config('settings.social_auth.google_client_secret')),
 		];
 	}
-	
+
 	/**
 	 * Get target URL
 	 *
@@ -68,32 +68,32 @@ class SocialController extends BaseController
 	 * @param string $provider
 	 * @return \Illuminate\Http\JsonResponse
 	 */
-	public function getProviderTargetUrl(string $provider): \Illuminate\Http\JsonResponse
+	public function getProviderTargetUrl(string $provider): \Illuminate\Http\JsonResponse | \Illuminate\Http\RedirectResponse
 	{
 		// Get the Provider and verify that if it's supported
 		if (!in_array($provider, $this->network)) {
 			return $this->respondNotFound();
 		}
-		
+
 		// Check if the Provider is enabled
 		$providerIsEnabled = (array_key_exists($provider, $this->networkChecker) && $this->networkChecker[$provider]);
 		if (!$providerIsEnabled) {
 			return $this->respondNotFound();
 		}
-		
+
 		// Redirect to the Provider's website
 		try {
-			return Socialite::driver($provider)->stateless()->redirect()->getTargetUrl();
+            return $this->respondSuccess(Socialite::driver($provider)->stateless()->redirect()->getTargetUrl());
 		} catch (\Throwable $e) {
 			$message = $e->getMessage();
 			if (empty($message)) {
 				$message = $this->serviceError;
 			}
-			
+
 			return $this->respondError($message);
 		}
 	}
-	
+
 	/**
 	 * Get user info
 	 *
@@ -108,21 +108,21 @@ class SocialController extends BaseController
 		if (!in_array($provider, $this->network)) {
 			return $this->respondNotFound();
 		}
-		
+
 		$data = [];
 		$extra = [];
-		
+
 		// API CALL - GET USER FROM PROVIDER
 		try {
 			// $userData = Socialite::driver($provider)->stateless()->user();
 			$token = request()->input('accessToken');
 			$userData = Socialite::driver($provider)->stateless()->userFromToken($token);
-			
+
 			// Data not found
 			if (!$userData) {
 				return $this->respondError(t('unknown_error_please_try_again'));
 			}
-			
+
 			// Email not found
 			if (!filter_var($userData->getEmail(), FILTER_VALIDATE_EMAIL)) {
 				return $this->respondError(t('email_not_found_at_provider', ['provider' => mb_ucfirst($provider)]));
@@ -132,17 +132,17 @@ class SocialController extends BaseController
 			if (empty($message)) {
 				$message = $this->serviceError;
 			}
-			
+
 			return $this->respondError($message);
 		}
-		
+
 		// Debug
 		// dd($userData);
-		
+
 		// DATA MAPPING
 		try {
 			$mapUser = [];
-			
+
 			// Get the user's Name (First Name & Last Name)
 			$mapUser['name'] = (isset($userData->name) && is_string($userData->name)) ? $userData->name : '';
 			if ($mapUser['name'] == '') {
@@ -160,16 +160,16 @@ class SocialController extends BaseController
 					}
 				}
 			}
-			
+
 			// Check if the user's email address has been banned
 			$bannedUser = Blacklist::ofType('email')->where('entry', $userData->getEmail())->first();
 			if (!empty($bannedUser)) {
 				return $this->respondError(t('This user has been banned'));
 			}
-			
+
 			// GET LOCAL USER
 			$user = User::withoutGlobalScopes([VerifiedScope::class])->where('provider', $provider)->where('provider_id', $userData->getId())->first();
-			
+
 			// CREATE LOCAL USER IF DON'T EXISTS
 			if (empty($user)) {
 				// Before... Check if user has not signup with an email
@@ -177,7 +177,7 @@ class SocialController extends BaseController
 				if (empty($user)) {
 					// Generate random password
 					$randomPassword = getRandomPassword(8);
-					
+
 					// Register the User (As New User)
 					$userInfo = [
 						'country_code'      => config('country.code', config('ipCountry.code')),
@@ -195,18 +195,18 @@ class SocialController extends BaseController
 					];
 					$user = new User($userInfo);
 					$user->save();
-					
+
 					// Send Generated Password by Email
 					try {
 						$user->notify(new SendPasswordAndVerificationInfo($user, $randomPassword));
 					} catch (\Throwable $e) {
 					}
-					
+
 					// Update Listings created by this email
 					if (isset($user->id) && $user->id > 0) {
 						Post::withoutGlobalScopes([VerifiedScope::class, ReviewedScope::class])->where('email', $userInfo['email'])->update(['user_id' => $user->id]);
 					}
-					
+
 					// Send Admin Notification Email
 					if (config('settings.mail.admin_notification') == 1) {
 						try {
@@ -219,7 +219,7 @@ class SocialController extends BaseController
 							return $this->respondError($e->getMessage());
 						}
 					}
-					
+
 				} else {
 					// Update 'created_at' if empty (for time ago module)
 					if (empty($user->created_at)) {
@@ -238,21 +238,21 @@ class SocialController extends BaseController
 				$user->phone_verified_at = now();
 				$user->save();
 			}
-			
+
 			// GET A SESSION FOR USER
 			if (auth()->loginUsingId($user->id)) {
 				$data['success'] = true;
 				$data['result'] = null;
-				
+
 				// Create the API access token
 				$deviceName = ucfirst($provider);
 				$token = $user->createToken($deviceName);
-				
+
 				$extra['authToken'] = $token->plainTextToken;
 				$extra['tokenType'] = 'Bearer';
-				
+
 				$data['extra'] = $extra;
-				
+
 				return $this->apiResponse($data);
 			} else {
 				return $this->respondError(t('Error on user\'s login.'));
@@ -262,7 +262,7 @@ class SocialController extends BaseController
 			if (empty($message)) {
 				$message = $this->serviceError;
 			}
-			
+
 			return $this->respondError($message);
 		}
 	}
