7603a5994616705e891f96d75b17aa58abcfa556
dllpl <nick.iv.dev@gmail.com>
2024-07-08 21:19:06 +0300

    Аккред

--- app/Http/Controllers/Web/Public/Account/SuperUserController.php
+++ app/Http/Controllers/Web/Public/Account/SuperUserController.php
@@ -28,7 +28,7 @@ class SuperUserController extends BaseController
             'data' => \DB::table('request_to_super as request')
                     ->where('user_id', $request->user()->id)
                     ->where('status', 1)
-                    ->leftJoin('request_to_super_status_list as status', 'status.status_id', '=', 'request.stats')
+                    ->leftJoin('request_to_super_status_list as status', 'status.status_id', '=', 'request.status')
                     ->select('request.status', 'status.name', 'status.desc')->first()?->status ?? null
         ]);
     }
